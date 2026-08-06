1b6e0c28080d2f8510901a92175543c62338eb4e
alex_d_d <alexey.denisov@outlook.com>
2026-08-07 00:33:19 +0300

    hw16

--- lesson15/hwadv.js
+++ lesson15/hwadv.js
@@ -1,15 +1,19 @@
-console.log({} == {}); //that is object placeholder, so it is false, because they are compared by reference in memory, not by value
-console.log({} === {}); //same reason
-console.log({}) //viewing the object, it is empty, but it is still an object
+// console.log({} == {}); //that is object placeholder, so it is false, because they are compared by reference in memory, not by value
+// console.log({} === {}); //same reason
+// console.log({}) //viewing the object, it is empty, but it is still an object
 
-console.log({} == "[object Object]"); // true, because the object is converted to a string "[object Object]" and then compared to the string "[object Object]"
-console.log("[object Object]");
-console.log({} === "[object Object]"); // false, because the object is not converted to a string, so they are different types and not equal
-console.log(typeof "[object Object]"); //helping to understand the previous line, it is a string, not an object
+// console.log({} == "[object Object]"); // true, because the object is converted to a string "[object Object]" and then compared to the string "[object Object]"
+// console.log("[object Object]");
+// console.log({} === "[object Object]"); // false, because the object is not converted to a string, so they are different types and not equal
+// console.log(typeof "[object Object]"); //helping to understand the previous line, it is a string, not an object
 
-console.log({} == true); // false, because the object is not converted to a boolean
-console.log(typeof {}); //helping to understand the previous line, it is an object, not a boolean
-console.log({} == false); // false, because the object is not converted to a boolean
+// console.log({} == true); // false, because the object is not converted to a boolean
+// console.log(typeof {}); //helping to understand the previous line, it is an object, not a boolean
+// console.log({} == false); // false, because the object is not converted to a boolean
 
-console.log({} == 0); // false, because the object is not converted to a number
-console.log({} == NaN); // false, because the object is not converted to NaN and we know that NaN is not equal to anything, even itself
+// console.log({} == 0); // false, because the object is not converted to a number
+// console.log({} == NaN); // false, because the object is not converted to NaN and we know that NaN is not equal to anything, even itself
+
+console.log(Number({}))
+console.log(String({}))
+console.log(String(true))
--- lesson15/test.js
+++ lesson15/test.js
@@ -1,16 +1,30 @@
-console.log(0.2 + 0.1 - 0.3);
-console.log(0.1);
-console.log(0.1 + 0.2);
-console.log(0.1 + 0.9);
-console.log(0.1 * 10);
-console.log(0.1 * 2);
-console.log(0.1 * 3);
-console.log(0.1 * 4);
-console.log(0.1 * 5);
-console.log(0.1 * 6);
-console.log(0.1 * 7);   
+// console.log(0.2 + 0.1 - 0.3);
+// console.log(0.1);
+// console.log(0.1 + 0.2);
+// console.log(0.1 + 0.9);
+// console.log(0.1 * 10);
+// console.log(0.1 * 2);
+// console.log(0.1 * 3);
+// console.log(0.3);
+// console.log(0.1 * 4);
+// console.log(0.1 * 5);
+// console.log(0.1 * 6);
+// console.log(0.1 * 7);   
 
-let number = 0.1;
-console.log(number);
-let res = number + 0.1;
-console.log(res)
+// let number = 0.1;
+// console.log(number);
+// let res = number + 0.1;
+// console.log(res)
+// let number1 = 0.2;
+// let number2 = 0.3;
+// console.log(number + number1 - number2)
+// console.log(number)
+// console.log(number + number1)
+
+
+const number = 0.1;
+console.log(number)
+const calc = 0.1 + 0.2;
+console.log(calc)
+console.log(0.1)
+console.log(number * 1);
