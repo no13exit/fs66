e1be5d30ae0ac812b4221667e21757babafc3cb8
alex_d_d <alexey.denisov@outlook.com>
2026-08-07 00:39:09 +0300

    fixed comments

--- lesson16/hw2.js
+++ lesson16/hw2.js
@@ -1,4 +1,4 @@
-let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //assuming "В файле 2 повторите пункты 1-5, но используйте методы массива splice и length соответственно." can be done here as well with another array
+let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
 array2.splice(0, 0, 0);
 array2.splice(11, 0, 11);
 console.log(array2);
