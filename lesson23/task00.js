b5bc4621d4f4951866e0cdcfe6aad8c4be5c8a40
alex_d_d <alexey.denisov@outlook.com>
2026-08-31 23:27:56 +0300

    hw23

---
+++ lesson23/task00.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import { GoogleGenAI } from "@google/genai";
+
+async function askAi(prompt) {
+  const genAi = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY
+  });
+
+  const response = await genAi.models.generateContent({
+    model: "gemini-3.5-flash-lite",
+    contents: prompt,
+    // as we need to locate specific part of the text, we need to set the temperature to 0
+    config: {
+        temperature: 0
+    }
+  });
+
+  return response.text;
+}
+
+async function main() {
+  const prompt =
+    "Напиши что такое REST API и как его использовать в JavaScript. Какие есть коды ответа сервера (200, 201, 404, 500) и что они означают. Приведи примеры использования методов GET, POST, PUT, DELETE и PATCH в JavaScript.";
+
+  const aiResponse = await askAi(prompt);
+  console.log("AI Response:", aiResponse);
+  return aiResponse;
+}
+
+const data = await main();    
+console.log(data);
+
+
+console.log(typeof data);
+
+// how many times REST is mentioned in the text?
+
+console.log('"REST" mentioned');
+let grep = data.split("REST").length - 1;
+console.log(grep);
+
+// how many times PUT is mentioned in the text?
+console.log('"PUT" mentioned');
+
+grep = data.split("PUT").length - 1;
+console.log(grep);
+
+// how many times GET is mentioned in the text?
+console.log('"GET" mentioned');
+
+grep = data.split("GET").length - 1;
+console.log(grep);
+
+// how many times POST is mentioned in the text?
+console.log('"POST" mentioned');
+
+grep = data.split("POST").length - 1;
+console.log(grep);
+
+// how many times DELETE is mentioned in the text?
+console.log('"DELETE" mentioned');
+grep = data.split("DELETE").length - 1;
+console.log(grep);
+
+// exract part about error codes "5.  **Status Codes** — ответ сервера (200 — ОК, 201 — Создано, 404 — Не найдено, 500 — Ошибка сервера)."
+
+
+const paragraphs = data.split(/\r?\n/);
+
+
+
+// print all paragraphs with their index to locate the part about error codes, commented later
+// for (let i = 0; i < paragraphs.length; i++) {
+//   const paragraph = paragraphs[i];
+//   console.log(i);
+//   console.log(paragraph);
+// }
+
+
+
+grep = paragraphs.slice(16,27);
+console.log(grep);
